6b88889ac0bfedd47ced624f0fb8eab96c2575f6
osalman1 <osalman@uwyo.edu>
2024-09-24 01:26:41 +0000

    i made changes

--- cosc1010-lab03.py
+++ cosc1010-lab03.py
@@ -19,42 +19,50 @@ print("Part One-----------------------------------------------------------------
 #Note this is the ONLY point where you need to declare the states list
 
 
+state = ["Wyoming", "Colorado", "Montana"]
 
 #print the entire list
 
+print(state)
 
 #now print the first element in the list
-
+print(state[0])
 
 #Print the last element using the syntax shown in class to access the final element (hint, think negatives)
-
+print(state[2])
 
 #Using an F-string to access the first and second element print the string "COLORADO is south of WYOMING", matching the casing provided
 
-
+print(f"{state[1].upper()}is south of{state[0].upper()}")
 
 
 print("Part Two------------------------------------------------------------------------")
 #Append the following states to your list: Washington, Oregon, California and print your list
+state.append("Washington")
+state.append("Oregon")
+state.append("California")
 
-
+print(state)
 #Again using the specific syntax mentioned in class overwrite the second to last element to be Maine, printing the list 
-
-
+state[4] = "Maine"
+print(state)
 #Insert the state Texas to be the third element in the list, again printing your list
-
-
+state.insert(2,"Texas")
+print(state)
 #Using the `del` statement remove the fourth item from the list, print your list 
-
-
+del state[3]
+print(state)
 #Remove Texas using its value, print the list
+state.remove("Texas")
+print(state)
 
 print("Part Three----------------------------------------------------------------------")
 #Temporarily sort your list, print it both sorted and unsorted 
-
-
+print(sorted(state))
+print(state)
 #Permanently sort your list in reverse order, printing it out
-
-
+state.reverse()
+print(state)
 #Using the reverse method reverse the list and print it
-
+state.reverse()
+print(state)
